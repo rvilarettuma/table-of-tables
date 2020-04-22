caeb144bfc4a6cb81309916fe68a698640af54fc
rvilarettuma <rvilaret@colostate.edu>
2020-04-22 13:04:32 -0600

    create Navbar component base

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
-import { Alert } from 'reactstrap';
+import Navigator from './Margins/Navigator';
 
 export default class App extends Component {
 
   render() {
     return (
       <div>
-        <Alert color="primary">Test Alert</Alert>
+        <Navigator/>
       </div>
     );
   }
---
+++ src/Margins/Navigator.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+// import { Navbar } from 'reactstrap';
+
+export default class Navigator extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    }
+  }
+
+  render() {
+    return (
+      <p>Test</p>
+    );
+  }
+}
