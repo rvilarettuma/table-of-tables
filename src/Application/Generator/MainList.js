ea8bc699ecae630ce57696a38b758dcc841c91ba
rvilarettuma <rvilaret@colostate.edu>
2020-04-23 17:41:48 -0600

    added categories to mainlist

--- src/Application/Generator/MainList.js
+++ src/Application/Generator/MainList.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-// import { ButtonDropdown, DropdownToggle, DropdownMenu, DropdownItem, ListGroup, ListGroupItem } from 'reactstrap'
+import { ListGroup, ListGroupItem } from 'reactstrap';
+import master_list from './Lists/master_list.json';
 
 export default class MainList extends Component {
 
@@ -10,15 +11,34 @@ export default class MainList extends Component {
       subcategory: 'Default Subcategory',
       dropdownOpen: false
     };
-
+    this.renderList = this.renderList.bind(this);
   }
 
   render() {
     return(
       <div>
-        <h1>List</h1>
+        <ListGroup>
+          {this.renderList()}
+        </ListGroup>
       </div>
     );
+  }
+
+  renderList() {
+    const mlist = JSON.parse(JSON.stringify({master_list}));
+    let items = [];
+    for (let i = 0; i < mlist.master_list.length; i++) {
+      Object.keys(mlist.master_list[i]).forEach(function(j) {
+        Object.keys(mlist.master_list[i][j]).forEach(function(k) {
+          if (mlist.master_list[i][j][k].header !== undefined) {
+            items.push(<ListGroupItem key={'lg_' + k + mlist.master_list[i][j][k].header}>{mlist.master_list[i][j][k].header}</ListGroupItem>)
+          } else {
+            console.log(mlist.master_list[i][j][k].name);
+          }
+        });
+      });
+    }
+    return(items);
   }
 
 }
