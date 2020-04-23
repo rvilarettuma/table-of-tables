0a2c75eb018e07732577d8e2be7611d4f6123842
rvilarettuma <rvilaret@colostate.edu>
2020-04-23 14:03:16 -0600

    added Constants.js

--- src/Application/Generator/MainList.js
+++ src/Application/Generator/MainList.js
@@ -1,18 +1,20 @@
 import React, { Component } from 'react';
 import { ListGroup } from 'reactstrap'
-// import items from './Lists/dl_list'
+import items from './Lists/dl_list'
 
 export default class MainList extends Component {
 
   constructor(props) {
     super(props);
+    this.renderListItems = this.renderListItems.bind(this);
   }
 
   render() {
     return(
       <ListGroup>
-
+        {this.renderListItems}
       </ListGroup>
     );
   }
+
 }
