9c767f31fe92ed5883b85a632ebe8fe2c674691b
rvilarettuma <rvilaret@colostate.edu>
2020-11-22 16:18:45 -0700

    Updates

---
+++ precache-manifest.1eb10ea903dac10ab1e404e2c2c59d4a.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3c5f2ae31b073240bf01ba9e95c9c3b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "24cf619304905329a003",
+    "url": "/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "27f98a45d4f073daf077",
+    "url": "/static/css/main.15a99e0f.chunk.css"
+  },
+  {
+    "revision": "24cf619304905329a003",
+    "url": "/static/js/2.90686cb4.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.90686cb4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "27f98a45d4f073daf077",
+    "url": "/static/js/main.f2c1bbf3.chunk.js"
+  },
+  {
+    "revision": "0b5c8007672e23bce394",
+    "url": "/static/js/runtime-main.ca09fe67.js"
+  },
+  {
+    "revision": "7b9e40f6db710fa59344e5dc8cc34539",
+    "url": "/static/media/SpaceGrotesk-Bold.7b9e40f6.woff"
+  },
+  {
+    "revision": "f5c67c0c4c54897c8b64ac0957fdc364",
+    "url": "/static/media/SpaceGrotesk-Light.f5c67c0c.woff"
+  },
+  {
+    "revision": "61a3f6134b0f3a5eb11b6b740ffcabaa",
+    "url": "/static/media/SpaceGrotesk-Medium.61a3f613.woff"
+  },
+  {
+    "revision": "fbbf6d98bfb9f3410a0de34728e9250e",
+    "url": "/static/media/SpaceGrotesk-Regular.fbbf6d98.woff"
+  },
+  {
+    "revision": "7b228459800eddb3e86861952daa4528",
+    "url": "/static/media/SpaceGrotesk-SemiBold.7b228459.woff"
+  },
+  {
+    "revision": "66d61ac300e4eb798374c3e84fc63783",
+    "url": "/static/media/rvt.66d61ac3.gif"
+  }
+]);
